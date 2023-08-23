3541f474ee3d8d005a2aeb2c05a68a06ca57bc50
jaycode8 <jamesmumo443@gmail.com>
2023-08-23 00:08:43 +0000

    updates

--- jay.js
+++ jay.js
@@ -1,4 +1,6 @@
- const container = document.querySelector(".container");
+const container = document.querySelector(".container");
+const input = document.getElementById('profile');
+const img = document.getElementById('imgDisplay');
 
 const toggleForm = () => {
     if (container.classList.contains('right-panel-active')) {
@@ -6,7 +8,16 @@ const toggleForm = () => {
     } else {
         container.classList.add("right-panel-active");
     }
-}
+};
+
+const handleFile = () => {
+    const selectedPhoto = input.files[0];
+    if (selectedPhoto) {
+        console.log(selectedPhoto)
+        const photoUrl = URL.createObjectURL(selectedPhoto)
+        img.src = photoUrl;
+    }
+};
 
 const handleSubmit = (e) => {
     e.preventDefault();
